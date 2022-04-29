380d0c8caf827e07dcd7e9fd96e559f577eed095
sizan378 <sizanmahmud08@gmail.com>
2022-04-29 14:52:41 +0600

    Django version update

--- Django_Project/settings.py
+++ Django_Project/settings.py
@@ -78,14 +78,21 @@ WSGI_APPLICATION = 'Django_Project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'blogsite',
+#         'USER': 'postgres',
+#         'PASSWORD': 'sizan',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'blogsite',
-        'USER': 'postgres',
-        'PASSWORD': 'sizan',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR,  'db.sqlite3'),
     }
 }
 
